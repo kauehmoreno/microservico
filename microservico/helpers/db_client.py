699f14fee04cb0744dceeb55ad3381fc4f21f7ae
Kaueh Moreno <kaueh.rodrigues@corp.globo.com>
2017-03-28 11:15:21 -0300

    Cria cliente bobo que irá servir apenas para instanciar e criar database_name do mongodb

---
+++ microservico/helpers/db_client.py
@@ -0,0 +1,19 @@
+# coding:utf-8
+import motor
+
+class Client(object):
+
+    def __init__(self, database_name):
+        self.database_name = database_name
+        self.__client = motor.motor_tornado.MotorClient(
+            'localhost', 27017
+        )
+        self.__create_database()
+
+    @property
+    def instance(self):
+        return self.__client
+
+    def __create_database(self):
+        self.__client[self.database_name]
+
