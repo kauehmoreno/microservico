9e6e8b4fddd80cf94710c9bf03f943ba9c495084
Kaueh Moreno <kaueh.rodrigues@corp.globo.com>
2017-03-28 22:52:01 -0300

    Define rota para receber post de matérias

--- microservico/urls.py
+++ microservico/urls.py
@@ -2,12 +2,14 @@
 import tornado.web
 from handlers.main_page import MainHandler
 from handlers.materia import Materia
+from handlers.operations import Integrator
 
 
 def urlspatterns():
     return tornado.web.Application([
         (r'/', MainHandler),
-        (r'/materia/', Materia)
+        (r'/materia/', Materia),
+        (r'/api/v1/(?P<operation>[\w]+)/?$', Integrator)
     ])
 
 
