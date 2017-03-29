30d209f39380a504445a4f44d4d5b8062a57c922
Kaueh Moreno <kaueh.rodrigues@corp.globo.com>
2017-03-29 00:37:38 -0300

    Adiciona web asyncronous

--- microservico/handlers/main_page.py
+++ microservico/handlers/main_page.py
@@ -4,6 +4,7 @@ import tornado.ioloop
 import tornado.web
 class MainHandler(tornado.web.RequestHandler):
 
+    @tornado.web.asynchronous
     def get(self):
         items = ['lala', 'kadu']
         self.render('home.html', title='Ola', items=items)
