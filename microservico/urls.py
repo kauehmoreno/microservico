baf3c7ea147844734006f2a8459a4a88e04fd2e2
Kaueh Moreno <kaueh.rodrigues@corp.globo.com>
2017-03-29 00:35:55 -0300

    Adiciona rota para static entregue pelo StaticFileHandler do tornado

--- microservico/urls.py
+++ microservico/urls.py
@@ -3,13 +3,14 @@ import tornado.web
 from handlers.main_page import MainHandler
 from handlers.materia import Materia
 from handlers.operations import Integrator
-
+from settings import static_path
 
 def urlspatterns():
     return tornado.web.Application([
         (r'/', MainHandler),
         (r'/materia/', Materia),
-        (r'/api/v1/(?P<operation>[\w]+)/?$', Integrator)
+        (r'/api/v1/(?P<operation>[\w]+)/?$', Integrator),
+        (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': static_path})
     ])
 
 
