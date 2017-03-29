f5a8cdadd7e3c87a857af462d22c5b77ba58783e
Kaueh Moreno <kaueh.rodrigues@corp.globo.com>
2017-03-29 15:01:52 -0300

    Cria urls para tratar queries de matéria e materias

--- microservico/urls.py
+++ microservico/urls.py
@@ -1,14 +1,15 @@
 # -- coding: utf-8 --
 import tornado.web
 from handlers.main_page import MainHandler
-from handlers.materia import Materia
+from handlers.materia import Materia, Materias
 from handlers.operations import Integrator
 from settings import static_path
 
 def urlspatterns():
     return tornado.web.Application([
         (r'/', MainHandler),
-        (r'/materia/', Materia),
+        (r'/materia/(?P<_id>[\w]+)/$', Materia),
+        (r'/materias/(?P<order_by>[\w]+)/(?P<limit>[\d+]+)/$', Materias),
         (r'/api/v1/(?P<operation>[\w]+)/?$', Integrator),
         (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': static_path})
     ])
