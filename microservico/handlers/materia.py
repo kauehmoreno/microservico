f74ed8bde890549efd943e062df5fab6cef3e313
Kaueh Moreno <kaueh.rodrigues@corp.globo.com>
2017-03-29 18:16:28 -0300

    Cria classe responsavel por responder queries de que buscar por todas as materias

--- microservico/handlers/materia.py
+++ microservico/handlers/materia.py
@@ -37,5 +37,34 @@ class Materias(tornado.web.RequestHandler):
 
     @tornado.web.asynchronous
     def get(self, *args, **kwargs):
-        self.write('OLLA MATERIAS')
-        self.finish()
+        DEFAULT_LIMIT = 20
+
+        order = {'esc': 1, 'desc': -1}
+        order_by = order.get(kwargs.get('order_by'))
+        limit = kwargs.get('limit')
+
+        db_client = DBbridge(self.application.db)
+
+        if not order_by:
+            order_by = -1
+
+        if int(limit) < 1:
+            limit = 20
+
+
+        loop = asyncio.get_event_loop()
+        try:
+            future = asyncio.ensure_future(db_client.filter(limit=int(limit), order_by=order_by))
+            result = loop.run_until_complete(future)
+        except Exception as e:
+            self.write(json.dumps({'error': str(e)}))
+            self.finish()
+        else:
+            if not result:
+                result = {}
+
+            self.set_header("Content-Type", "application/json; charset=UTF-8")
+
+            self.write(json.dumps(result))
+            self.finish()
+
